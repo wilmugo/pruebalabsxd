508e18381e1beb08cc618789bb4673f5ea084d57
Wilfredo Murillo <wilmugo@gmail.com>
2020-02-17 19:43:50 -0400

    prueba3 para heroku

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/angular-build"));
+app.use(express.static(__dirname + "/dist/listadesplegable"));
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "angular-build", "index.html"));
+  res.sendFile(path.join(__dirname, "/dist/listadesplegable/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
